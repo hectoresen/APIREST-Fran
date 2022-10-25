8f34202555fcf9cd48a927efdccb5abae77c1986
hectoresen <inforeshector@gmail.com>
2022-10-25 13:13:46 +0200

    FEAT: Adding User entity, model & interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -9,7 +11,8 @@ import { AppService } from './app.service';
       envFilePath: [
         '.prod.env', '.test.env', '.env'
       ]
-    })
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
