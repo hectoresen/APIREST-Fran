47274f48c2375fda6089301eb7844418110070af
Francisco Manuel Fernández Pino <71704294+FranciscoFer99@users.noreply.github.com>
2022-10-25 11:59:33 +0200

    datos env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.prod.env', '.test.env', '.env'
+      ]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
