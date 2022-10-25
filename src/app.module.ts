3328714c99a2465dc64e11dce9465144580fbc7d
Francisco Manuel Fernández Pino <71704294+FranciscoFer99@users.noreply.github.com>
2022-10-25 12:23:26 +0200

    añadir postgra y eliminar mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { AppService } from './app.service';
       envFilePath: [
         '.prod.env', '.test.env', '.env'
       ]
-    })
+    }),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
