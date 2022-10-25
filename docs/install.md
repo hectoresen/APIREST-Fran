84415312aec6089cb14daada1674964862a2d06c
Francisco Manuel Fernández Pino <71704294+FranciscoFer99@users.noreply.github.com>
2022-10-25 13:18:30 +0200

    Revert "Feature/users"

--- docs/install.md
+++
@@ -1,9 +0,0 @@
-Instalación
--------
-
-```bash
-1º. docker-compose -f docker-compose.yaml up -d
-```
-```bash
-2º. docker-compose up
-```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: [
-        '.prod.env', '.test.env', '.env'
-      ]
-    }),
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
